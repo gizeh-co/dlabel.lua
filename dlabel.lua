81c452d6fb54a3578026d0321d4d041a5eb031e6
nostripledev <52100294+nostripledev@users.noreply.github.com>
2021-10-19 21:55:52 +0200

    Update dlabel.lua

--- dlabel.lua
+++ dlabel.lua
@@ -261,10 +261,8 @@ end
 
 function PANEL:GetHack(args,hack)
    self.m_bHackFunction = args
-   if self.m_bHackFunction!=tobool("false") then 
+   if self.m_bHackFunction!=false then 
       print(hack)
-   else 
-      print("please specify argument in GetHack")
    end 
 end
 
